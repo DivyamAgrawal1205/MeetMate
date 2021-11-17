6d34fff64df67b0257cb8053953baa17ca24eccd
DivyamAgrawal1205 <83166347+DivyamAgrawal1205@users.noreply.github.com>
2021-11-17 12:11:51 +0530

    Update sp2txt.js
    To reduce reduntant or useless code.

--- Extension Code/sp2txt.js
+++ Extension Code/sp2txt.js
@@ -1,24 +1,14 @@
-var speech = true;
-                window.SpeechRecognition = window.SpeechRecognition
-                                || window.webkitSpeechRecognition;
-          
-                const recognition = new SpeechRecognition();
-                recognition.interimResults = true;
-                const words = document.querySelector('.words');
-                words.appendChild(b);
-          
-                recognition.addEventListener('result', e => {
-                    const transcript = Array.from(e.results)
-                        .map(result => result[0])
-                        .map(result => result.transcript)
-                        .join('')
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+recognition.continuous = true;
+var words = document.querySelector('.words');
+words.appendChild(b);
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
 
-                        document.getElementById("b").innerHTML = transcript;
-                        console.log(transcript);
-                });
-                  
-                if (speech == true) {
-                    recognition.continuous = true
-                    recognition.start();
-                    recognition.addEventListener('end',recognition.continuous = true, recognition.start);
-                }
+        document.getElementById("b").innerHTML = transcript;
+        console.log(transcript);
+});
