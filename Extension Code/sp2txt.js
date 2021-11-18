c062b04dd528b0e2a38536eb714e7ee37d7085ac
Aayush-21 <aayushsinghpanwar.btcse20@pec.edu.in>
2021-11-18 15:56:49 +0530

    commenting sp2txt

--- Extension Code/sp2txt.js
+++ Extension Code/sp2txt.js
@@ -1,6 +1,6 @@
 var SpeechRecognition = window.webkitSpeechRecognition; //initialising the speechRecognition object from webkitSpeechRecognition API 
 var recognition = new SpeechRecognition(); //initialising recognition variable from SpeechRecognition object
-recognition.continuous = true;
+recognition.continuous = true; // making speech recognition continuous
 var words = document.querySelector('.words');
 words.appendChild(b);
 recognition.addEventListener('result', e => {
