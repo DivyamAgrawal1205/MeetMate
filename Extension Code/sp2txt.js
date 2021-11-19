40b42b9941989a0ecf5f7d8c75e2c24af573e540
DivyamAgrawal1205 <agradivyam1205@gmail.com>
2021-11-19 22:06:53 +0530

    Commenting sp2txt.js to be more clear in some line

--- Extension Code/sp2txt.js
+++ Extension Code/sp2txt.js
@@ -1,14 +1,14 @@
 var SpeechRecognition = window.webkitSpeechRecognition; //initialising the speechRecognition object from webkitSpeechRecognition API 
 var recognition = new SpeechRecognition(); //initialising recognition variable from SpeechRecognition object
 recognition.continuous = true; // making speech recognition continuous
-var words = document.querySelector('.words'); //
-words.appendChild(textArea);
+var words = document.querySelector('.words'); //fectching the dividion in index.html with class "words"
+words.appendChild(textArea);// inserting textarea as a node in division as a class "words"
 //mapping the speech recognition result from an Array to a transcript variable
 recognition.addEventListener('result', e => {
-    const transcript = Array.from(e.results)
+    const transcript = Array.from(e.results) // creating a transcript array using results from the Speech Recognition
         .map(result => result[0])
         .map(result => result.transcript)
-        .join('') //joining the result by spaces to create representable statements
+        .join('') //joining the transcript array into string by spaces to create representable statements
 
-        document.getElementById("textArea").innerHTML = transcript; //inserting result in the txt area
+        document.getElementById("textArea").innerHTML = transcript; //inserting result in the text area
 });
