92ced0fe772383640534da9a7803d1f83b62fbb3
DivyamAgrawal1205 <agradivyam1205@gmail.com>
2021-11-21 15:57:01 +0530

    implementing PDFgen,js

--- Extension Code/PDFgen.js
+++ Extension Code/PDFgen.js
@@ -1,5 +1,5 @@
 function goPDF (){ // This function gets activated  when we click Download button calls html2pdf API and saves the contents of the element of id = "topdf"
-    html2pdf()
-    .from(document.getElementById("topdf"))
-    .save();
+    
+    var pdf =html2pdf().from(document.getElementById("topdf")).save();
+    return pdf;
     }
