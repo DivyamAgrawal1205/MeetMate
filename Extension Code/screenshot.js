882966f0bb02e43d49f9453bbd6516b25f848341
AryanJindal <94097852+AryanJindal@users.noreply.github.com>
2021-11-22 21:38:30 +0530

    Update screenshot.js

--- Extension Code/screenshot.js
+++ Extension Code/screenshot.js
@@ -10,6 +10,10 @@ async function TakeScreenShot(){
     shotMap = await shotRecorder.grabFrame();       // storing all the image data in a variable
     shotStream.stop();                             // stopping the recording as the work is done to grab an image
 
+    
+    // To display the picture to the user in the pdf , we have to draw the image(reason explained in PDFgen.js),
+    // also we used canvas element as it only shows itself in the pdf
+    //As we have to display a new image everytime when the Screenshot Button is clicked , we are adding a new canvas element.  
     shotCanvas = document.createElement('canvas');
     shotCanvas.width = 800;
     shotCanvas.height = 450;
