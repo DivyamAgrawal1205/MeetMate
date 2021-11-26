1f38d0307de52d9572b6debec93766f071c1aa58
DivyamAgrawal1205 <83166347+DivyamAgrawal1205@users.noreply.github.com>
2021-11-26 20:10:17 +0530

    Completing Attention detection module

--- MeetMate/attIrisTracking.js
+++ MeetMate/attIrisTracking.js
@@ -24,3 +24,37 @@ setInterval(function(){
     else {
         score = 0; 
         Status = "Very Less Attentive!";
+        var audioCtx = new (window.AudioContext || window.webkitAudioContext || window.audioContext);
+        var oscillator = audioCtx.createOscillator();
+        var gainNode = audioCtx.createGain();
+        oscillator.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
+        oscillator.start(audioCtx.currentTime);
+        oscillator.stop(audioCtx.currentTime + ((150 || 500) / 1000));
+
+}
+
+    total_time +=2;
+    totalAttScore += score;
+
+    attPercentage = ((totalAttScore/total_time)*100).toFixed(1) +"%";
+    
+    let currentDate = new Date();
+    let TimeString = currentDate.getHours()  + ":"+  currentDate.getMinutes() +":"+ currentDate.getSeconds();
+    
+    time_array.push(TimeString);
+    attentive_array.push(score);
+
+    var attentivenessTrace = {
+        x : time_array,
+        y : attentive_array,
+        fill : 'tonexty',
+        type : 'scatter'
+    }
+    
+    var attData = [attentivenessTrace];
+    Plotly.newPlot('attentivenessChart',attData);
+
+    para = document.getElementById("p1");
+    para.innerHTML = Status+ "  Attentiveness Score :"+attPercentage;
+},2000)
